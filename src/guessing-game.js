736652453a75aa44cc50227a973ba7400589aff0
Vladimir <v.milashevsky@gmail.com>
2022-08-06 18:58:13 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        return (Math.ceil((this.minimum + this.maximum) / 2));
     }
 
     lower() {
-
+        this.maximum = this.guess();
+        return this.maximum;
     }
 
     greater() {
-
+        this.minimum = this.guess();
+        return this.minimum;
     }
 }
 
